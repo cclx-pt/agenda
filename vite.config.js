5ff9fb4c114e2e7df7f637703047864a660fb41a
Ricardo Ribeiro <riribeiro+microsoft@microsoft.com>
2026-06-25 00:40:49 +0100

    shadcn/ui: fundacao (Tailwind v3 + tokens neutros --sc- + kit ui base)

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,6 +7,11 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
     test: {
       globals: true,
       environment: 'jsdom',
